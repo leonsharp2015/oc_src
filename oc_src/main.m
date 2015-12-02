fe6e9994cd501e0f304744e94e88b053fed58b95
lanou3g <lanou3g@lanou3gdemac-mini-4.local>
2015-12-02 08:28:28 +0800

    Initial Commit

---
+++ oc_src/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  oc_src
+//
+//  Created by lanou3g on 15/12/2.
+//  Copyright © 2015年 lanou3g. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
