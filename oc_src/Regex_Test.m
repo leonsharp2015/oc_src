a0c113e52bf400c114aeac4b296bbaa7ce120b8c
lanou3g <lanou3g@lanou3gdemac-mini-4.local>
2015-12-05 09:26:07 +0800

    2

---
+++ oc_src/Regex_Test.m
@@ -0,0 +1,22 @@
+//
+//  Regex_Test.m
+//  oc_src
+//
+//  Created by lanou3g on 15/12/5.
+//  Copyright © 2015年 lanou3g. All rights reserved.
+//
+
+#import "Regex_Test.h"
+
+@implementation Regex_Test
+-(void) test
+{
+    //1.正则表达式与NSPredicate连用
+    BOOL chk=NO;
+    NSString *temp=@"12j456";
+    NSString *strReg=@"^[0-9]+$";
+    NSPredicate *pre=[NSPredicate predicateWithFormat:@"SELF MATCHES %@",strReg];
+    chk=[pre evaluateWithObject:temp];
+    //2.string 
+}
+@end
--- oc_src/main.m
+++ oc_src/main.m
@@ -8,14 +8,16 @@
 
 #import <Foundation/Foundation.h>
 #import "ArraySort.h"
+#import "Regex_Test.h"
 
 int main(int argc, const char * argv[])
 {
 
-    ArraySort *sort=[[ArraySort alloc] init ];
-    [sort test];
+    //ArraySort *sort=[[ArraySort alloc] init ];
+    //[sort test];
     
    
-   
+    Regex_Test *reg=[[Regex_Test alloc]init];
+    [reg test];
     
 }
