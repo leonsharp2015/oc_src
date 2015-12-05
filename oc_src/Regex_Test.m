0d63e9b0b0227cbcd6b4f39be0c87783a8c75191
lanou3g <lanou3g@lanou3gdemac-mini-4.local>
2015-12-05 15:27:23 +0800

    1

--- oc_src/Regex_Test.m
+++ oc_src/Regex_Test.m
@@ -11,12 +11,11 @@
 @implementation Regex_Test
 -(void) test
 {
-    //1.正则表达式与NSPredicate连用
+    
     BOOL chk=NO;
-    NSString *temp=@"12j456";
-    NSString *strReg=@"^[0-9]+$";
-    NSPredicate *pre=[NSPredicate predicateWithFormat:@"SELF MATCHES %@",strReg];
-    chk=[pre evaluateWithObject:temp];
+    NSString *temp=@"";
+    NSString *strReg=@"";
+
     //2.string rangeOfString
     NSString *str1 = @"can you A speak English";
     NSString *str = @"A";
@@ -32,23 +31,14 @@
         NSLog(@"range ：%@", [temp substringWithRange:range]);
     }
     
-    //4.正则表达式
-    NSString *searchText = @"ad12366ff";
-    NSError *error = NULL;
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"^*[0-9]*$" options:NSRegularExpressionCaseInsensitive error:&error];
-    NSTextCheckingResult *result = [regex firstMatchInString:searchText options:0 range:NSMakeRange(0, [searchText length])];
-    if (result) {
-        NSLog(@"===%@", [searchText substringWithRange:result.range]);
-    }
+
     
     
 }
 
 -(void) test_regex //NSRegularExpression类
 {
-    //numberOfMatchesInString
-    
-    //1.
+    //1.matchesInString拆分字符串为数组
     NSString *temp=@"http://www.baidu.com&http://www.sina.com&http://www.sohu.com";
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:
                                   @"http://([\\w-]+\\.)+[\\w-]+(/[\\w- ./?%&=]*)?" options:NSRegularExpressionCaseInsensitive error:nil];
@@ -58,20 +48,18 @@
     array = [regex matchesInString:temp options:0 range:NSMakeRange(0, [temp length])];
     for (NSTextCheckingResult* b in array)
     {
-        //subStr 是每个和表达式匹配好的字符串。
-        subStr = [temp substringWithRange:b.range];
+        subStr = [temp substringWithRange:b.range];//subStr 是每个和表达式匹配好的字符串。
         NSLog(@" str 1 is %@",subStr);
     }
     
-    //2.拆分字符串中的数字
+    //拆分字符串中的数字,共3组
     temp=@"123ef78682ji90";
     regex = [NSRegularExpression regularExpressionWithPattern:
                                   @"[0-9]+"
                                   options:NSRegularExpressionCaseInsensitive error:nil];
     array = nil;
     array = [regex matchesInString:temp options:0 range:NSMakeRange(0, [temp length])];
-//    NSInteger count=[regex numberOfMatchesInString:temp options:0
-//                                             range:NSMakeRange(0, [temp ength])];
+//    NSInteger count=[regex numberOfMatchesInString:temp options:0                                  range:NSMakeRange(0, [temp ength])];
     subStr = nil;
     for (NSTextCheckingResult* b in array)
     {
@@ -80,22 +68,16 @@
         NSLog(@" str 1 is %@",subStr);
     }
 
-    //3.firstMaches
-    
-    //组装一个字符串，需要把里面的网址解析出来
-    NSString *urlString=@"<meta/><link/><title>1Q84 BOOK1</title></head>                            ";
+    //2.firstMaches
+    //将字符串里面的字符串解析出来
+    NSString *urlString=@"<meta/><link/><title>1Q84 BOOK1</title></head>";
     NSError *error;
-    
-    
     regex = [NSRegularExpression regularExpressionWithPattern:
                         @"(?<=title\\>).*(?=</title)"
                        options:0 error:&error];
     
-    NSInteger count=[regex numberOfMatchesInString:urlString options:0
-                                             range:NSMakeRange(0, [urlString length])];
-    
-    
-    if (regex != nil) {
+    if (regex != nil)
+    {
         NSTextCheckingResult *firstMatch=[regex firstMatchInString:urlString options:0 range:NSMakeRange(0, [urlString length])];
         
         if (firstMatch)
@@ -103,10 +85,30 @@
             NSRange resultRange = [firstMatch rangeAtIndex:0];
             NSString *result=[urlString substringWithRange:resultRange];//从urlString当中截取数据
             NSLog(@"->%@<-",result);
-        } 
+        }
+        //替换
+        NSString *result=[regex stringByReplacingMatchesInString:urlString options:0 range:NSMakeRange(0, [urlString length]) withTemplate:@"aaa"];
         
     }
     
+    //将字符串里面的第一组数字字符串解析出来
+    temp = @"jj12366kkk9854";
+    regex = [NSRegularExpression regularExpressionWithPattern:@"[0-9]+" options:NSRegularExpressionCaseInsensitive error:&error];
+    
+    NSTextCheckingResult *result = [regex firstMatchInString:temp options:0 range:NSMakeRange(0, [temp length])];
+    if (result)
+    {
+        NSLog(@"===%@", [temp substringWithRange:result.range]);
+    }
+    
+    //3.验证.与谓词一起用
+    BOOL chk=NO;
+    temp=@"12456";
+    NSString *strReg=@"^[0-9]+$";
+    NSPredicate *pre=[NSPredicate predicateWithFormat:@"SELF MATCHES %@",strReg];
+    chk=[pre evaluateWithObject:temp];
+    
+
     
     
     
